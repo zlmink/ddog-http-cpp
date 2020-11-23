7b1586e5177f52518a139e21ea9cff8a12735c18
Zachary Mink <zacharylmink@gmail.com>
2020-11-22 22:50:07 -0800

    added libcxx option to conan install

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 mkdir -p build && cd build
 
-conan install .. --build=missing
+conan install ..  -s compiler=gcc -s compiler.libcxx=libstdc++11 --build=missing
 
 # Configure
 cmake -DCMAKE_BUILD_TYPE=Debug ..
